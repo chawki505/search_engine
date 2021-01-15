a182801865ac9f5800d7c9b52b911064a9a078e0
Chawki CHOUIB <chouibc@gmail.com>
2021-01-15 22:35:06 +0100

    new version create corpus function

---
+++ src/corpus.py
@@ -0,0 +1,115 @@
+import xml.etree.ElementTree as ET
+from xml.dom import minidom
+import html
+
+import csv
+import time
+import os
+import io
+
+from src.utils import valid_category
+
+PATH_WIKI_XML = '../data/'
+FILENAME_WIKI = 'frwiki-20201201-pages-articles-multistream.xml'
+# FILENAME_WIKI = 'frwiki10000.xml'
+FILENAME_ARTICLES = 'pages-filtre.csv'
+FILENAME_CORPUS = 'corpus.xml'
+
+pathWikiXML = os.path.join(PATH_WIKI_XML, FILENAME_WIKI)
+pathPages = os.path.join(PATH_WIKI_XML, FILENAME_ARTICLES)
+pathCorpus = os.path.join(PATH_WIKI_XML, FILENAME_CORPUS)
+
+
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
+
+
+def strip_tag_name(t):
+    idx = t.rfind("}")
+    if idx != -1:
+        t = t[idx + 1:]
+    return t
+
+
+def create_corpus():
+    total_pages_count = 0
+    total_filtre_pages_count = 0
+    title = None
+    root = None
+
+    start_time = time.time()
+
+    with io.open(pathPages, 'w') as pagesFH:
+
+        page_writer_csv = csv.writer(pagesFH, quoting=csv.QUOTE_MINIMAL)
+
+        page_writer_csv.writerow(['id', 'title'])
+
+        for event, elem in ET.iterparse(pathWikiXML, events=('start', 'end')):
+            tname = strip_tag_name(elem.tag)
+            if event == 'start':
+                if tname == 'mediawiki':
+                    root = ET.Element(tname)
+                if tname == 'page':
+                    ns = 0
+                    title = ''
+                    id = -1
+                    inrevision = False
+                    content = ''
+                elif tname == 'revision':
+                    # Do not pick up on revision id's
+                    inrevision = True
+            else:
+                if tname == 'title':
+                    title = elem.text
+                elif tname == 'id' and not inrevision:
+                    id = int(elem.text)
+                elif tname == 'ns':
+                    ns = int(elem.text)
+                elif tname == 'text' and inrevision:
+                    content = elem.text
+
+                elif tname == 'page':
+                    total_pages_count += 1
+
+                    if ns == 0 and valid_category(content):
+                        total_filtre_pages_count += 1
+                        page_elem = ET.SubElement(root, tname)
+
+                        title_elem = ET.SubElement(page_elem, 'title')
+                        title_elem.text = title
+
+                        id_elem = ET.SubElement(page_elem, 'id')
+                        id_elem.text = str(id)
+
+                        text_elem = ET.SubElement(page_elem, 'text')
+                        text_elem.text = content
+
+                        page_writer_csv.writerow([id, title])
+
+                    if total_pages_count > 1 and (total_pages_count % 10000) == 0:
+                        print("Current pages : {:,}".format(total_pages_count))
+                        print("Current pages filtre: {:,}".format(total_filtre_pages_count))
+
+                elem.clear()
+            # limit corpus to 500K pages
+            if total_filtre_pages_count == 500000:
+                break
+    xmlstr = minidom.parseString(ET.tostring(root, encoding='unicode')).toprettyxml()
+
+    with io.open(pathCorpus, 'w') as corpusFH:
+        # corpusFH.write(html.unescape(xmlstr))
+        corpusFH.write(xmlstr)
+
+    elapsed_time = time.time() - start_time
+    print("Total pages: {:,}".format(total_pages_count))
+    print("Total pages filtre: {:,}".format(total_filtre_pages_count))
+    print("Elapsed time: {}".format(hms_string(elapsed_time)))
+
+
+if __name__ == '__main__':
+    create_corpus()
---
+++ src/utils.py
@@ -0,0 +1,61 @@
+import re
+
+
+def valid_category(page):
+    """
+        Test if our page talk about our topic
+        :param page
+        :return: true if the page talk about our topic
+    """
+    categorie_re = re.compile(r'\[+Catégorie:(.*)([Ll](ittérature)|ittéraire)|(ivre)(.*)\]+')
+
+    match = None
+    try:
+        match = categorie_re.search(page)
+    except TypeError:
+        print(page)
+    return match is not None
+
+
+def delete_brackets(s):
+    """
+    Delete all brackets in out page content
+    :param s: our page content
+    :return new page content without brackets
+    """
+    stack = []
+    i = 0
+    size = len(s)
+    while i < size - 1:
+        c = s[i]
+        if i == size - 2:
+            return s
+        if c == '{' and s[i + 1] == '{':
+            stack.append(('{', i))
+            i += 2
+        if c == '}' and s[i + 1] == '}':
+            if len(stack) == 1:
+                start_index = stack.pop()[1]
+                s = s[: start_index] + s[i + 2:]
+                i = start_index
+                size = len(s)
+            else:
+                if stack:
+                    stack.pop()
+                else:
+                    s = s[: i] + s[i + 2:]
+                    size = len(s)
+                i += 2
+        else:
+            i += 1
+    return s
+
+
+def namespace(element):
+    """
+    :param The XML element
+    :return The namespace of element
+        Exemple namespace("'{http://maven.apache.org/POM/4.0.0}project'" -> {http://maven.apache.org/POM/4.0.0}
+    """
+    m = re.match(r'\{.*\}', element.tag)
+    return m.group(0) if m else ''

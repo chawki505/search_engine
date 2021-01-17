b10b5d30f152a55de4f590b306a2a3dfdd50c4b6
Chawki CHOUIB <chouibc@gmail.com>
2021-01-17 15:15:58 +0100

    fix create corpus

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ def valid_category(page):
         :param page
         :return: true if the page talk about our topic
     """
-    categorie_re = re.compile(r'\[+Catégorie:(.*)('
+    categorie_re = re.compile(r'\[+Catégorie:(.*?)('
                               r'([lL]itt[ée]rature(s)?)|'
                               r'([lL]itt[ée]raire(s)?)|'
                               r'([lL]ivre(s)?)|'
@@ -17,33 +17,47 @@ def valid_category(page):
                               r'([pP]o[èe]me(s)?)|'
                               r'([pP]o[èe]te(s)?(sse(s)?)?)|'
                               r'([hH]omonymie(s)?)|'
-                              r'([ÉEée]crivain)|'
-                              r'([rR]oman)|'
+                              #  r'([ÉEée]cri((vain)|(ture)|(t(e)?))(s)?)|'
+                              r'([ÉEée]crivain(s)?)|'
+                              r'([Pp]resse(s)?)|'
+                              r'([lL]inguistique(s)?)|'
+                              r'([rR]oman((cier)|(ci[eè]re))?(s)?)|'
+                              r'([dD]ocument(aire)?(s)?)|'
+                              r'([eE]ncyclop[ée]die(s)?)|'
+                              r'([vV]ers)|'
+                              r'([lL]ang((ue)|(age))(s)?)|'
                               r'([aA]rticle(s)?)|'
                               r'([aA]uteur)|'
                               r'([rR]echerche)|'
                               r'([oO]uvrage(s)?)|'
-                              r'([tT]exte)|'
+                              #     r'([tT]exte)|'
                               r'([nN]arratologie)|'
                               r'([rR][eé]cit)|'
                               r'([pP]hilologie)|'
                               r'([rR]evue)|'
                               r'([pP]rix)|'
-                              r'([tT]raduction)|'
-                              r'([tT]raducteur)|'
+                              r'([tT]raduct((ion)|(eur))(s)?)|'
                               r'([ÉEée]diteur)|'
                               r'([ÉEée]tude)|'
-                              r'(Œuvre)|([oO]euvre)|'
-                              r'([bB]iblioth[èe]que)|'
-                              r'([mM]us[eé]e)'
-                              r')(.*)\]+')
+                              r'(Œuvre(s)?)|'
+                              r'([oO]euvre(s)?)|'
+                              r'([bB]iblioth[èe]que(s)?)'
+                              # r'([mM]us[eé]e(s)?)|'
+                              #   r'([cC]ulture(s)?)|'
+                              #      r'([aA]rt)'
+                              r')(.*?)\]+')
 
-    match = None
-    try:
+    # categorie_re = re.compile(r'\[+Catégorie:(.*)((Littérature(s)?)|(littéraire(s)?))(.*)\]+')
+    # categorie_re = re.compile(r'\[+Catégorie:(.*)((Littérature(s)?)|(Littérature(s)?))(.*)\]+')
+
+    if page:
         match = categorie_re.search(page)
-    except TypeError:
-        print(page)
-    return match is not None
+
+        if match:
+            # print("matching ", match, "status :", match.groups())
+            return match is not None
+
+    return False
 
 
 def delete_brackets(s):
@@ -82,9 +96,24 @@ def delete_brackets(s):
 
 def namespace(element):
     """
-    :param The XML element
-    :return The namespace of element
+    :param element: the XML element
+    :return the namespace of element
         Exemple namespace("'{http://maven.apache.org/POM/4.0.0}project'" -> {http://maven.apache.org/POM/4.0.0}
     """
     m = re.match(r'\{.*\}', element.tag)
     return m.group(0) if m else ''
+
+
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
+
+
+def strip_tag_name(t):
+    idx = t.rfind("}")
+    if idx != -1:
+        t = t[idx + 1:]
+    return t

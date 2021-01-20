e31408c8537f6993b02d3b61ca46275c635ca886
Benjamin Viau <benjamin_viau@yahoo.com>
2021-01-20 14:08:33 +0100

    Graph generator and link finder

--- src/main.py
+++ src/main.py
@@ -141,6 +141,34 @@ def clean(text):
 
     return text
 
+def get_links(page_text):
+    """
+    :param page_text: Page texte
+    :return:
+    The list of externals link of the page
+    """
+    import re
+    l = re.findall('\[\[.*?\]\]', page_text)
+    return [s[2:-2] for s in l]
+
+
+def pages_to_matrix(l):
+    """
+    edge : [[Title]] in page content
+    node : page id
+    :param l: list of pair containing (title, page content)
+    :return:
+        adjacency matrix of the web graph
+    """
+    m_len = len(l)
+    matrix = [[0 for i in range(m_len)] for j in range(m_len)]
+    for i, (title, page) in enumerate(l):
+        links = get_links(page)
+        for link in links:
+            link_id = next(i for i, (title, page) in enumerate(l) if title == link)
+            matrix[i][link_id] = 1/len(links)
+    return matrix
+
 
 if __name__ == '__main__':
     file = "../data/corpus.xml"

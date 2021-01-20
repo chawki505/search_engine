3f1f830693dacaaf9f2aa50c6a9c34c7c7517d1f
Benjamin Viau <benjamin_viau@yahoo.com>
2021-01-20 14:08:33 +0100

    Graph generator and link finder

--- src/main.py
+++ src/main.py
@@ -1,37 +1,38 @@
 from parse import parse
 
 
-def matrix_to_cli(matrix, size):
+def get_links(page_text):
     """
-    :param matrix: squared adjacency matrix
-    :param size: size of @matrix
+    :param page_text: Page texte
     :return:
-        C L I tuple
+    The list of externals link of the page
     """
-    C = []
-    L = []
-    I = []
-    for i in range(size):
-        current_row = []
-        for j in range(size):
+    import re
+    l = re.findall('\[\[.*?\]\]', page_text)
+    return [s[2:-2] for s in l]
 
-            elem = matrix[i][j]
-            if elem == 0:
-                continue
-            else:
-                current_row.append(elem)
-                I.append(j)
-                C.append(elem)
-        if len(current_row) > 0:
-            if not L:
-                L.append(0)
-                L.append(len(current_row))
-            else:
-                L.append(L[-1] + len(current_row))
-    return C, L, I
+
+def pages_to_matrix(l):
+    """
+    edge : [[Title]] in page content
+    node : page id
+    :param l: list of pair containing (title, page content)
+    :return:
+        adjacency matrix of the web graph
+    """
+    m_len = len(l)
+    matrix = [[0 for i in range(m_len)] for j in range(m_len)]
+    for i, (title, page) in enumerate(l):
+        links = get_links(page)
+        for link in links:
+            link_id = next(i for i, (title, page) in enumerate(l) if title == link)
+            matrix[i][link_id] = 1 / len(links)
+    return matrix
 
 
 if __name__ == '__main__':
-    file = "../data/corpus2.xml"
+    file = "../data/corpus.xml"
 
     mylist = parse(file)
+
+    print(mylist)

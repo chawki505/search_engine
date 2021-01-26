d62d544a3bb31fd047ab345e7fa7bfbeeb53faec
leet <budzkoal@gmail.com>
2021-01-26 19:06:22 +0100

    serialize

--- src/main.py
+++ src/main.py
@@ -2,34 +2,6 @@ from parse import parse
 import math
 
 
-def matrix_to_cli(matrix, size):
-    """
-    :param matrix: squared adjacency matrix
-    :param size: size of @matrix
-    :return:
-        C L I tuple
-    """
-    C = []
-    L = []
-    I = []
-    for i in range(size):
-        current_row = []
-        for j in range(size):
-
-            elem = matrix[i][j]
-            if elem == 0:
-                continue
-            else:
-                current_row.append(elem)
-                I.append(j)
-                C.append(elem)
-        if len(current_row) > 0:
-            if not L:
-                L.append(0)
-                L.append(len(current_row))
-            else:
-                L.append(L[-1] + len(current_row))
-    return C, L, I
 
 
 def get_links(page_text):

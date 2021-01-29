911cfb30b270db1fc348f7466ac6ab4d01a1d391
Chawki CHOUIB <chouibc@gmail.com>
2021-01-29 17:21:29 +0100

    fix clean function (#18)
    * dictionnary
    
    * serialize
    
    * gitignore upddate
    
    * TF-IDF
    
    * fix clean function
    
    * create dict patch
    
    * split parse function in new file
    
    * Graph generator and link finder
    
    * Little optimization
    
    * Adding id in tuple
    
    * Replace matrix_to_cli and page_to_matrix by page_to_cli
    
    * conflict fix
    
    * serialize
    
    * gitignore upddate
    
    * TF-IDF
    
    * create dict patch
    
    * Graph generator and link finder
    
    * Adding id in tuple
    
    * Replace matrix_to_cli and page_to_matrix by page_to_cli
    
    * conflict fix
    
    * serialize
    
    * gitignore upddate
    
    * TF-IDF
    
    * create dict patch
    
    * dictionnary
    
    * serialize
    
    * fix conflit
    
    * fix conflit
    
    * split parse function in new file
    
    * Replace matrix_to_cli and page_to_matrix by page_to_cli
    
    * serialize
    
    * dictionnary
    
    * serialize
    
    * fix conflit
    
    * update code
    
    Co-authored-by: leet <budzkoal@gmail.com>
    Co-authored-by: Benjamin Viau <benjamin_viau@yahoo.com>
    Co-authored-by: leethater <35769770+leethater@users.noreply.github.com>

--- src/main.py
+++ src/main.py
@@ -1,116 +1,5 @@
-import xml.etree.ElementTree as ET
-
-import string
-import nltk
-import spacy
-import re
+from parse import parse, nlp
 import math
-
-from utils import delete_brackets, serialize, deserialize
-
-nltk.download('stopwords', quiet=True)
-from nltk.corpus import stopwords
-
-mystopwords = stopwords.words('french')
-nlp = spacy.load("fr_core_news_lg")
-
-
-def parse_text_page(text):
-    """
-    Parse text to only get main parts of the text ("== Title ==" paragraphs)
-    :param text:
-    :return:
-        A clean "string" text
-    """
-    # TODO : Parse links ([], {}..etc) and tags
-    is_in_subtitle = False
-    sub_title_re = "=== Bibliographie ===|== Notes et références ==|== Voir aussi =="
-    final_text = ""
-
-    text = delete_brackets(text)
-
-    for line in text.split("\n"):
-        if re.match(sub_title_re, line):
-            is_in_subtitle = True
-            # continue ??
-        if re.match("== .* ==", line) and not is_in_subtitle:
-            is_in_subtitle = False
-        if not is_in_subtitle:
-            final_text += line + "\n"
-    return final_text
-
-
-def parse(file_name):
-    """
-    :param file_name:
-        XML file containing pages data
-    :return:
-        List of tuple containing (id, title, content) for each page
-    """
-
-    page_list = []
-    total_pages_count = 0
-
-    id = None
-    title = None
-    content = None
-
-    for event, elem in ET.iterparse(file_name, events=('start', 'end')):
-
-        tname = elem.tag
-
-        if event == 'start':
-
-            if tname == 'page':
-                title = ''
-                id = -1
-                content = ''
-
-        else:
-
-            if tname == 'title':
-                title = elem.text
-
-            elif tname == 'id':
-                id = int(elem.text)
-
-            elif tname == 'text':
-                content = elem.text
-
-            elif tname == 'page':
-                total_pages_count += 1
-                page_list.append((id, title, parse_text_page(content)))
-
-            elem.clear()
-
-    return page_list
-
-
-def clean(text):
-    """
-    :param text: text to clean
-    :return:
-        Apply lemmeization to @text and return it
-    """
-
-    # make str low
-    text = text.lower()
-
-    # TODO: fix contrations in french
-    # remove contraction
-    # text = contractions.fix(text)
-
-    # remove punctuations
-    punctuations_reg = re.compile('[%s]' % re.escape(string.punctuation))
-    text = punctuations_reg.sub(r'', text)
-
-    # remove stopwords
-    text = ' '.join([elem for elem in text.split() if elem not in mystopwords])
-
-    # Lemmatization
-    text = ' '.join([x.lemma_ for x in nlp(text)])
-
-    return text
 
 
 def get_links(page_text):
@@ -154,6 +43,7 @@ def pages_to_cli(l):
                 L.append(L[-1] + edge_nb)
     return C, L, I
 
+
 def create_dict(page_list):
     """
     :param page_list: list of pages to parse
@@ -165,12 +55,12 @@ def create_dict(page_list):
     for (id, title, content) in page_list:
         title_lemmatized = [x.lemma_ for x in nlp(title)]
         for word in title_lemmatized:
-            if word not in dico_title.keys(): # word not in dict
-                dico_title[word] = ({id : 1}, 0)
-            else: # word in dict
-                if id not in dico_title[word][0].keys(): # page is not in list
+            if word not in dico_title.keys():  # word not in dict
+                dico_title[word] = ({id: 1}, 0)
+            else:  # word in dict
+                if id not in dico_title[word][0].keys():  # page is not in list
                     dico_title[word][0][id] = 1
-                else : # page already in list
+                else:  # page already in list
                     dico_title[word][0][id] += 1
         for word in content:
             if word not in dico_text.keys():
@@ -180,25 +70,24 @@ def create_dict(page_list):
                     dico_text[word][0][id] = 1
                 else:  # page already in list
                     dico_text[word][0][id] += 1
-    dico_title.update({key: value for key, value in sorted(list(dico_text.items()), key=lambda item: len(item[1][0].items()))[-10000:]})
-    tf_norm = dict() # normalized TF
+    dico_title.update({key: value for key, value in
+                       sorted(list(dico_text.items()), key=lambda item: len(item[1][0].items()))[-10000:]})
+    tf_norm = dict()  # normalized TF
     for word, (occ_dic, idf) in dico_title.items():
-        for pageid, freq in occ_dic.items() :
+        for pageid, freq in occ_dic.items():
             if freq > 0:
                 if pageid not in tf_norm.keys():
-                    tf_norm[pageid] = (1 + math.log10(freq))**2
-                else :
-                    tf_norm[pageid] += (1 + math.log10(freq))**2
-    # writing IDF and normalized TF 
+                    tf_norm[pageid] = (1 + math.log10(freq)) ** 2
+                else:
+                    tf_norm[pageid] += (1 + math.log10(freq)) ** 2
+    # writing IDF and normalized TF
     for word in dico_title.keys():
-        idf = math.log10(len(page_list)/len(dico_title[word][0].keys()))
+        idf = math.log10(len(page_list) / len(dico_title[word][0].keys()))
         dico_title[word] = (dico_title[word][0], idf)
         for page, tf in dico_title[word][0].items():
-            dico_title[word][0][page] = tf/math.sqrt(tf_norm[page])
+            dico_title[word][0][page] = tf / math.sqrt(tf_norm[page])
     return dico_title
-    
-            
+
 
 if __name__ == '__main__':
-    # file = "../data/corpus.xml"
-    # file = "../data/frwiki10000.xml"
+    pass
